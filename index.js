b4ffbe800ed34b1cea3dc2984a685eee88810b96
ashwanikumar1809kr-cmd <ashwanikumar1809@gmail.com>
2025-09-14 07:45:39 +0530

    GET API complete

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 /**
- * start the sever
+ * Start the server
  */
-const express = require('express')
+const express = require('express');
+const app = express();
+
+//Stitching the route here
+require("./routes/idea.routes")(app);
 
 
-const app = express()
+app.listen(8000, ()=>{
+    console.log("App started on the port num ", 8000);
+});
 
-//stitching the route here
-require("./routes/idea.routes")(app)
-
-
-app.listen(8000, ()=> {
-    console.log("App started on the port num", 8000)
-})
--- models/ideas.model.js
+++ models/ideas.model.js
@@ -4,21 +4,20 @@ module.exports = {
         id : 1,
         idea_name : "Great Idea",
         author_name : "ashwani",
-        idea_description : "This is one of the finest idea i ever had"
+        idea_description : "This is one of the finest idea I ever had"
     },
-    
+
     2 : {
         id : 2,
         idea_name : "Great Second Idea",
-        author_name : "mohan",
-        idea_description : "This is one of the  second most finest idea i ever had"
+        author_name : "Prashant",
+        idea_description : "This is one of the second most finest idea I ever had"
     },
-    
     3 : {
         id : 3,
-        idea_name : "Class Idea",
-        author_name : "prashant",
-        idea_description : "This is idea came form the student "
-}
+        idea_name : "Class idea",
+        author_name : "Sunny",
+        idea_description : "This idea came from the students of my batch "
+    }
 
-} 
+};
